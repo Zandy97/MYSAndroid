ce17f6bc3f3acb2bc8c0a9a9ed98fafaf75666db
zandy97 <rla743456@gmail.com>
2021-11-03 01:23:54 +0900

    BottomNavigate & Fragment 생성

--- app/src/main/java/com/example/ymsandroid/MainActivity.java
+++ app/src/main/java/com/example/ymsandroid/MainActivity.java
@@ -9,21 +9,14 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private  TextView tvId, tvPWD;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tvId = findViewById(R.id.tvId);
-        tvPWD = findViewById(R.id.tvPwd);
-
         Intent intent = getIntent();
-        String userId = intent.getStringExtra("userId");
+        String userId = intent.getStringExtra("userID");
         String userPwd = intent.getStringExtra("userPWD");
 
-        tvId.setText(userId);
-        tvPWD.setText(userPwd);
     }
 }
