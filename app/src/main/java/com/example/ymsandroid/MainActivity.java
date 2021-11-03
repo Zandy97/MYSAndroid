ea2b369352042eb02b7fb348e59bee5e09883508
zandy97 <rla743456@gmail.com>
2021-11-03 15:05:09 +0900

    BottomNavigate & Fragment 생성

--- app/src/main/java/com/example/ymsandroid/MainActivity.java
+++ app/src/main/java/com/example/ymsandroid/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.ymsandroid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.view.MenuItem;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    CalendarFragment calendarFragment;
+    ScheduleFragment scheduleFragment;
+    ToDoListFragment toDoListFragment;
+    UserFragment userFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +24,32 @@ public class MainActivity extends AppCompatActivity {
         String userId = intent.getStringExtra("userID");
         String userPwd = intent.getStringExtra("userPWD");
 
+        calendarFragment = new CalendarFragment();
+        scheduleFragment = new ScheduleFragment();
+        toDoListFragment = new ToDoListFragment();
+        userFragment = new UserFragment();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, toDoListFragment).commit();
+        BottomNavigationView bottom_menu = findViewById(R.id.bottom_menu);
+        bottom_menu.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.itmTdl:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, toDoListFragment);
+                        return true;
+                    case R.id.itmCal:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, calendarFragment);
+                        return true;
+                    case R.id.itmSch:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, scheduleFragment);
+                        return true;
+                    case R.id.itmUser:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, userFragment);
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
